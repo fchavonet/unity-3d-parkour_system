bd37e611ab31747d45600f45209f8f24d756cb34
fchavonet <fchavonet@mailo.com>
2024-11-06 22:30:33 +0100

    Creating a simple camera controller.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Player transform reference.
+    [Space(10)]
+    public Transform player;
+
+    // Camera settings.
+    [Space(10)]
+    public float rotationSpeed = 2f;
+    public float distance = 5;
+    public float minVerticalAngle = -20f;
+    public float maxVerticalAngle = 45f;
+    public Vector2 framingOffset;
+
+    // Inversion flags.
+    [Space(10)]
+    public bool invertX;
+    public bool invertY;
+
+    // Rotation variables.
+    private float rotationX;
+    private float rotationY;
+
+    // // Inversion values.
+    private float invertXValue;
+    private float invertYValue;
+
+    // Initialize cursor settings.
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update camera rotation and position.
+    private void Update()
+    {
+        // Set inversion values.
+        invertXValue = (invertX) ? -1 : 1;
+        invertYValue = (invertY) ? -1 : 1;
+
+        // Handle vertical rotation.
+        rotationX += Input.GetAxis("Mouse Y") * invertYValue * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+
+        // Handle horizontal rotation.
+        rotationY += Input.GetAxis("Mouse X") * invertXValue * rotationSpeed;
+
+        // Calculate target rotation and camera position.
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+        var focusPosition = player.position + new Vector3(framingOffset.x, framingOffset.y);
+
+        // Apply the position and rotation to the camera.
+        transform.SetPositionAndRotation(focusPosition - targetRotation * new Vector3(0, 0, distance), targetRotation);
+    }
+}
